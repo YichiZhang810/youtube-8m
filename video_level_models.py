b62fea2c913eb8d4ddc48b573afb8b31a93353a7
YichiZhang810 <yichizhangmax@utexas.edu>
2017-05-02 08:49:27 -0500

    Add files via upload

--- video_level_models.py
+++ video_level_models.py
@@ -170,7 +170,7 @@ class CnnModel(models.BaseModel):
         activation=tf.nn.relu)
 
     # Pooling Layer #1
-    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 1], strides=2)
 
     # Convolutional Layer #2 and Pooling Layer #2
     conv2 = tf.layers.conv2d(
@@ -179,7 +179,7 @@ class CnnModel(models.BaseModel):
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
-    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 1], strides=2)
 
     # Dense Layer
     pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
