394e1861336059bea19349e2879aa974f7210e28
YichiZhang810 <yichizhangmax@utexas.edu>
2017-05-02 08:48:08 -0500

    Add files via upload

--- video_level_models.py
+++ video_level_models.py
@@ -169,6 +169,28 @@ class CnnModel(models.BaseModel):
         padding="same",
         activation=tf.nn.relu)
 
+    # Pooling Layer #1
+    pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+    # Convolutional Layer #2 and Pooling Layer #2
+    conv2 = tf.layers.conv2d(
+      inputs=pool1,
+      filters=64,
+      kernel_size=[5, 5],
+      padding="same",
+      activation=tf.nn.relu)
+    pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+
+    # Dense Layer
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
+    dropout = tf.layers.dropout(
+      inputs=dense, rate=0.4, training=mode == learn.ModeKeys.TRAIN)
+
+    # Logits Layer
+    logits = tf.layers.dense(inputs=dropout, units=10)
+
+
 
     output = slim.fully_connected(
         model_input, vocab_size, activation_fn=tf.nn.sigmoid,
